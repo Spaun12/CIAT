109b49d58a3ea4b8d2dad2bcd3b2eefefb9c6e8e
Michael D. Connell Jr <85039481+Spaun12@users.noreply.github.com>
2023-02-06 13:49:01 -0600

    Week1 additional

---
+++ ASD101A Python Fundamentals/Part2/Assignment/Week1/Python2 Week 1 Assignment1 Part 1 Michael Connell.py
@@ -0,0 +1,10 @@
+#Week1 Python2 Assignment1 Part Michael Connell
+
+def greetings(school_name):
+  print("Welcome to " + school_name + "!")
+
+# Prompt the user to enter the school name
+school_name = input("Enter the name of the school: ")
+
+# Function call
+greetings(school_name)
