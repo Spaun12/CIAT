97dbc5cb1b26d6515d20c12a6c4467b915f97627
Michael D. Connell Jr <85039481+Spaun12@users.noreply.github.com>
2023-02-13 02:28:42 -0600

    updates

---
+++ ASD101A Python Fundamentals/Part2/Discussion Board/Week2/Week2 Discussion Board Michael Connell.py
@@ -0,0 +1,23 @@
+#Week2 Discussion Board Michael Connell
+
+# Define a function that takes two parameters and returns the sum of the parameters
+def add(a, b):
+    # Calculate the sum of the parameters
+    result = a + b
+    
+    # Return the result
+    return result
+
+# Define a void function that takes one parameter and prints it
+def print_parameter(param):
+    # Print the parameter
+    print(param)
+
+# Call the add function and store the result in a variable
+result = add(2, 3)
+
+# Print the result
+print("The sum of 2 and 3 is:", result)
+
+# Call the print_parameter function
+print_parameter("Hello, World!")
---
+++ ASD101A Python Fundamentals/Part2/Take Away Journal/Python2 Week1 Take Away Journal module.py
@@ -0,0 +1,11 @@
+# Python2 Week1 Take Away Journal module_example Michael Connell
+x = 5
+
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+print(square(x))
+print(cube(x))
