59606d25bb0224de16637e4e6b6c58881877a4b5
Michael D. Connell Jr <85039481+Spaun12@users.noreply.github.com>
2023-02-21 15:14:05 -0600

    additional files

---
+++ ASD101A Python Fundamentals/Part2/Assignment/Week3/Part2/Week 3 Assignment Part 2b Michael Conn.py
@@ -0,0 +1,11 @@
+# Week 3 Assignment Part 2b Michael Connell 
+# Create a sample dictionary
+dct = {'John': 30, 'Michael': 47, 'David': 25, 'James': 26}
+
+# Check if 'James' key exists in the dictionary
+if 'James' in dct:
+    # If 'James' key exists, print the associated value
+    print("The value associated with 'James' is:", dct['James'])
+else:
+    # If 'James' key doesn't exist, print a message
+    print("'James' is not in the dictionary.")
