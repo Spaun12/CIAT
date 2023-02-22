9ffa3c79fd4274bebfa0ffcae611e2545407cd2c
Michael D. Connell Jr <85039481+Spaun12@users.noreply.github.com>
2023-02-22 17:47:42 -0600

    updates

---
+++ ASD101A Python Fundamentals/Part2/Take Away Journal/Week3/Week3 Take Away Journal 2 Michael Connell.py
@@ -0,0 +1,18 @@
+#Week3 Take Away Journal 2 Michael Connell
+# Accept user input for the string
+my_string = input("Enter a string: ")
+
+# Access individual characters using indexing
+# Here, we use indices to access the first, second,
+# and last characters of the string.
+# The first character has an index of 0, the second character has an index of 4,
+# and the last character has an index of -1.
+first_char = my_string[0]
+second_char = my_string[4]
+last_char = my_string[-1]
+
+# Print the results
+print("First character: {}".format(first_char))
+print("Second character: {}".format(second_char))
+print("Last character: {}".format(last_char))
+
