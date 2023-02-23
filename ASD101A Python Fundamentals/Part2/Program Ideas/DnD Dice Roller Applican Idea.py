cc45df6247baddb4af3275ed77c7125f792e2637
Michael D. Connell Jr <85039481+Spaun12@users.noreply.github.com>
2023-02-23 12:13:53 -0600

    Update DnD Dice Roller Applican Idea.py

--- ASD101A Python Fundamentals/Part2/Program Ideas/DnD Dice Roller Applican Idea.py
+++ ASD101A Python Fundamentals/Part2/Program Ideas/DnD Dice Roller Applican Idea.py
@@ -1,5 +1,4 @@
 #DnD Dice Roller Applican Idea
-
 # Import the random module to generate random numbers
 import random
 # Define a dictionary mapping dice types (e.g. "d6") to the number of sides on the dice
@@ -11,31 +10,45 @@ dice_dict = {
     "d12": 12,
     "d20": 20
 }
-# Prompt the user to roll all of their dice at once, or individually
-roll_all = input("Would you like to roll all of your dice at once? (y/n) ")
-if roll_all == "y":
-    # If the user selects "y", roll all the dice
-    rolls = []
-    for dice_type, sides in dice_dict.items():
-        roll = random.randint(1, sides)
-        rolls.append(roll)
-    # Print the resulting rolls to the user
-    print(f"Rolls: {rolls}")
-else:
-    # If the user selects "n", prompt for the number and type of dice, and generate rolls for each die individually using a for loop
-    dice = int(input("How many dice? "))
-    if dice <= 0:
-        print("You must roll at least one die.")
-        quit()
-    dice_type = input("Enter the type of dice you want to roll (e.g. d6): ")
-    try:
-        sides = dice_dict[dice_type]
-    except KeyError:
-        print("Invalid dice type. Please enter a valid type (e.g. d6).")
-        quit()
-    rolls = []
-    for i in range(dice):
-        face = random.randint(1, sides)
-        rolls.append(face)
-    # Print the resulting rolls to the user
-    print(f"Rolls: {rolls}")
+# Keep rolling until the user chooses to quit
+while True:
+    # Prompt the user to roll all of their dice at once, or individually
+    roll_all = input("Would you like to roll all of your dice at once? (y/n) ")
+    if roll_all == "y":
+        # If the user selects "y", roll all the dice
+        rolls = {}
+        for dice_type, sides in dice_dict.items():
+            roll = random.randint(1, sides)
+            rolls[dice_type] = roll
+        # Print the resulting rolls to the user
+        print("Rolls:")
+        for dice_type, roll in rolls.items():
+            print(f"{dice_type} x 1: {roll}")
+    else:
+        # If the user selects "n", prompt for the number and type of dice, and generate rolls for each die individually using a for loop
+        dice = int(input("How many dice? "))
+        if dice <= 0:
+            print("You must roll at least one die.")
+            continue
+        dice_types = []
+        for i in range(dice):
+            dice_type = input(f"Enter the type of dice {i+1}: ")
+            if dice_type not in dice_dict:
+                print("Invalid dice type. Please enter a valid type (e.g. d6).")
+                break
+            dice_types.append(dice_type)
+        else:
+            rolls = {}
+            for dice_type in dice_types:
+                sides = dice_dict[dice_type]
+                roll = random.randint(1, sides)
+                rolls[dice_type] = roll
+            # Print the resulting rolls to the user
+            print("Rolls:")
+            for dice_type, roll in rolls.items():
+                print(f"{dice_type} x 1: {roll}")
+            continue
+    # Ask the user if they want to try again
+    play_again = input("Roll again? (y/n) ")
+    if play_again == "n":
+        break
