9b53124528916227cf845658c7d57bca1c483751
Michael D. Connell Jr <85039481+Spaun12@users.noreply.github.com>
2023-04-18 20:55:08 -0500

    Week 1 Updates

---
+++ ASD102B_22/Week1/HandsOn/Ch1/project04/project01-04.js
@@ -0,0 +1,20 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Michael D. Connell Jr.
+    Date: 2023-04-18  
+
+    Filename: project01-04.js
+*/
+
+// Define variables for home and work addresses
+var homeStreet = "1 Main St.";
+var homeCity = "Sicilia";
+var homeState = "MA";
+var homeCode = "02103";
+
+var workStreet = "15 Oak Ln.";
+var workCity = "Central City";
+var workState = "MA";
+var workCode = "02104";
