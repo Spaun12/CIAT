8b99063915cb799347054b9a962735dd4eb14476
Michael D. Connell Jr <85039481+Spaun12@users.noreply.github.com>
2023-04-26 15:09:30 -0500

    homework updates

--- ASD102B_22/Week2/HandsOn/Week 2 Hands-On Excercise 1/project5-2/project05-02.js
+++ ASD102B_22/Week2/HandsOn/Week 2 Hands-On Excercise 1/project5-2/project05-02.js
@@ -4,32 +4,36 @@
       Project 05-02
 
       Project to move images between a photo bucket and photo list.
-      Author: Michael D. Connell Jr.
-      Date: 2023-04-26  
+      Author: 
+      Date:   
 
       Filename: project05-02.js
 */
 
-// Declare the required variables
-let images = document.getElementsByTagName("img");
-let photoBucket = document.getElementById("photo_bucket");
-let photoList = document.getElementById("photo_list");
+// Wrap the entire script in a window.onload event to ensure the script runs after the page has loaded completely
+window.onload = function () {
+   // Declare the required variables
+   let images = document.getElementsByTagName("img");
+   let photoBucket = document.getElementById("photo_bucket");
+   let photoList = document.getElementById("photo_list");
 
-// Iterate through all items in the images collection
-for (let i = 0; i < images.length; i++) {
-   // Add onclick event handler for each image
-   images[i].onclick = function () {
-      // Determine if the image should be moved to the photo list or back to the photo bucket
-      if (this.parentElement.id === "photo_bucket") {
-         // Move image from photo bucket to photo list
-         let newItem = document.createElement("li");
-         photoList.appendChild(newItem);
-         newItem.appendChild(this);
-      } else {
-         // Move image from photo list back to photo bucket
-         let oldItem = this.parentElement;
-         photoBucket.appendChild(this);
-         oldItem.parentElement.removeChild(oldItem);
-      }
-   };
-}
+   // Iterate through all items in the images collection
+   for (let i = 0; i < images.length; i++) {
+      // Add onclick event handler for each image
+      images[i].onclick = function () {
+         // Determine if the image should be moved to the photo list or back to the photo bucket
+         if (this.parentElement.id === "photo_bucket") {
+            // Move image from photo bucket to photo list
+            let newItem = document.createElement("li");
+            photoList.appendChild(newItem);
+            newItem.appendChild(this);
+         } else {
+            // Move image from photo list back to photo bucket
+            let oldItem = this.parentElement;
+            photoBucket.appendChild(this);
+            oldItem.parentElement.removeChild(oldItem);
+         }
+      };
+   }
+};
+
