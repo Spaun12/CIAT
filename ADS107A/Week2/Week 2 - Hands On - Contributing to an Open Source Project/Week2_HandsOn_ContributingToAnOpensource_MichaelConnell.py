e2fc78af2e1ce056cefaf453ca70202c70960d2f
Michael D. Connell Jr <85039481+Spaun12@users.noreply.github.com>
2023-11-25 17:58:48 -0600

    Update Week2_HandsOn_ContributingToAnOpensource_MichaelConnell.py

--- ADS107A/Week2/Week 2 - Hands On - Contributing to an Open Source Project/Week2_HandsOn_ContributingToAnOpensource_MichaelConnell.py
+++ ADS107A/Week2/Week 2 - Hands On - Contributing to an Open Source Project/Week2_HandsOn_ContributingToAnOpensource_MichaelConnell.py
@@ -1,5 +1,6 @@
 # Week 2 - Hands-On - Contributing to an Open Source Project
 # Simple Python script for learning the process
+# Update for enter name:
 
 def main():
    # print("Hello, Python! This is my contribution to the open-source project.")
