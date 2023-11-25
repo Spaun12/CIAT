4e300f054eb137d31fbdffa0bd3a9c15482e676f
Michael D. Connell Jr <85039481+Spaun12@users.noreply.github.com>
2023-11-25 17:57:28 -0600

    Update for Hands Week2
    Added function to capture name and show in welcome message.

--- ADS107A/Week2/Week 2 - Hands On - Contributing to an Open Source Project/Week2_HandsOn_ContributingToAnOpensource_MichaelConnell.py
+++ ADS107A/Week2/Week 2 - Hands On - Contributing to an Open Source Project/Week2_HandsOn_ContributingToAnOpensource_MichaelConnell.py
@@ -2,7 +2,11 @@
 # Simple Python script for learning the process
 
 def main():
-    print("Hello, Python! This is my contribution to the open-source project.")
+   # print("Hello, Python! This is my contribution to the open-source project.")
+   name = input("Enter your name: ")
+    
+    # Greet the user with their name
+   print(f"Hello, {name}! Welcome to the open-source project.")
 
 if __name__ == "__main__":
     main()
